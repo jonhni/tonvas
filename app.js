85ae3f94939ce51e26e6cfae1a0bb7355efc95dd
Jon Nilsson <jon.h.nilsson@gmail.com>
2015-11-29 15:14:57 +0100

    log server port

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(serveStatic(path.join(__dirname, 'public')));
-app.listen(3000);
+app.listen(3000, () =>{
+  console.log("Server running on port: ", port);
+});
